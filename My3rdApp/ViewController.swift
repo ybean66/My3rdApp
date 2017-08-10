8e0cd96aacd903e0e6fd9257e2d891b3d3536c85
ybean66 <ybean.chen@gmail.com>
2017-08-10 15:21:46 +0800

    Initial Commit

---
+++ My3rdApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My3rdApp
+//
+//  Created by Brad Chen on 2017/8/10.
+//  Copyright © 2017年 Brad Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
